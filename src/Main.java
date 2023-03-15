1cfafe0f2a2d4b19c07e1cf0f2eec31a8ba096b6
Federico Arias <fede.ariass33@gmail.com>
2023-03-15 15:31:19 -0300

    ejercicio 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numeroWhile = 0;
+        int numeroIf = 2;
+        int numeroFor = 0;
+        var estacion = "invierno";
+
+        if(numeroIf < 0){
+            System.out.println("es positivo");
+        }else if(numeroIf > 0){
+            System.out.println("es negativo");
+        }else{
+            System.out.println("es 0");
+        }
+
+        while (numeroWhile < 3){
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        };
+
+        do {
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }while (numeroWhile < 3);
+
+        for (int i =0;i<3;i++){
+            numeroFor++;
+            System.out.println(numeroFor);
+        };
+
+        switch (estacion){
+            case "primavera":
+                System.out.println("es primavera");
+                break;
+            case "verano":
+                System.out.println("es verano");
+                break;
+            case "otoño":
+                System.out.println("es otoño");
+                break;
+            case "invierno":
+                System.out.println("es invierno");
+                break;
+            default:
+                System.out.println("la variable no es una estacion");
+        }
+
     }
 }
